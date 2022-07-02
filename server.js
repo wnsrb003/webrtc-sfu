661257d60e2fa4ee70d57ca638d92a4c5a62ef7b
박준규 <wnsrb003@naver.com>
2022-07-02 20:58:19 +0900

    add stun, turn server domain

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ const WebSocketServer = WebSocket.Server;
 let serverOptions = {
     listenPort: 3000,
     useHttps: true,
-    httpsCertFile: '/home/ubuntu/simple_sfu/ssl/cert/ssl.crt',
-    httpsKeyFile: '/home/ubuntu/simple_sfu/ssl/key/ssl.key',
+    // httpsCertFile: '/home/ubuntu/simple_sfu/ssl/cert/ssl.crt',
+    // httpsKeyFile: '/home/ubuntu/simple_sfu/ssl/key/ssl.key',
+    httpsCertFile: './ssl/cert/ssl.crt',
+    httpsKeyFile: './ssl/key/ssl.key'
 };
 
 let sslOptions = {};
@@ -28,7 +30,7 @@ if (serverOptions.useHttps) {
 }
 
 let webServer = null;
-if(serverOptions.useHttps) {
+if (serverOptions.useHttps) {
     webServer = https.createServer(sslOptions, app);
     webServer.listen(serverOptions.listenPort);
 } else {
@@ -54,10 +56,40 @@ function handleTrackEvent(e, peer, ws) {
 function createPeer() {
     let peer = new webrtc.RTCPeerConnection({
         iceServers: [
-            { 'urls': 'stun:stun.stunprotocol.org:3478' },
-            { 'urls': 'stun:stun.l.google.com:19302' },
-	    { 'urls': 'stun:stun1.l.google.com:19302' },
-	    { 'urls': 'stun:stun2.l.google.com:19302' },
+            { urls: 'stun:stun01.sipphone.com' },
+            { urls: 'stun:stun.ekiga.net' },
+            { urls: 'stun:stun.fwdnet.net' },
+            { urls: 'stun:stun.ideasip.com' },
+            { urls: 'stun:stun.iptel.org' },
+            { urls: 'stun:stun.rixtelecom.se' },
+            { urls: 'stun:stun.schlund.de' },
+            { urls: 'stun:stun.l.google.com:19302' },
+            { urls: 'stun:stun1.l.google.com:19302' },
+            { urls: 'stun:stun2.l.google.com:19302' },
+            { urls: 'stun:stun3.l.google.com:19302' },
+            { urls: 'stun:stun4.l.google.com:19302' },
+            { urls: 'stun:stunserver.org' },
+            { urls: 'stun:stun.softjoys.com' },
+            { urls: 'stun:stun.voiparound.com' },
+            { urls: 'stun:stun.voipbuster.com' },
+            { urls: 'stun:stun.voipstunt.com' },
+            { urls: 'stun:stun.voxgratia.org' },
+            { urls: 'stun:stun.xten.com' },
+            {
+                urls: 'turn:numb.viagenie.ca',
+                credential: 'muazkh',
+                username: 'webrtc@live.com'
+            },
+            {
+                urls: 'turn:192.158.29.39:3478?transport=udp',
+                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                username: '28224511:1379330808'
+            },
+            {
+                urls: 'turn:192.158.29.39:3478?transport=tcp',
+                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                username: '28224511:1379330808'
+            }
         ]
     });
 
